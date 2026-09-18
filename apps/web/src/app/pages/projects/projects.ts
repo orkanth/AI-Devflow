880bd399d162fdadcea38f8f508a9f882b44ed2a
raviFalcorp <98960739+raviFalcorp@users.noreply.github.com>
2026-09-18 18:15:24 +0530

    added project pages

---
+++ apps/web/src/app/pages/projects/projects.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'df-projects',
+  imports: [],
+  templateUrl: './projects.html',
+  styleUrl: './projects.css',
+})
+export class Projects {}
